39f792db7b8c3cd1ee14e32468e307c2127701bd
Eduard Čuba <ecuba@redhat.com>
2017-12-25 15:04:25 +0100

    toogle led

--- deploy.sh
+++ deploy.sh
@@ -3,15 +3,9 @@ echo "Copying source files"
 sudo ampy --port /dev/ttyUSB0 put main.py
 sudo ampy --port /dev/ttyUSB0 put index.html
 
-exit
-
-# update bootstrap dependencies - this may take few minutes
-
-echo "Copying javascript"
-sudo ampy --port /dev/ttyUSB0 put js
-
 echo "Copying CSS"
-sudo ampy --port /dev/ttyUSB0 put css
+
+sudo ampy --port /dev/ttyUSB0 put styles.css
 
 echo "Copying icon"
 sudo ampy --port /dev/ttyUSB0 put favicon.ico
--- main.py
+++ main.py
@@ -1,79 +1,69 @@
+import machine
 import network
+import socket
+import os
 
 ap_if = network.WLAN(network.AP_IF)
 ap_if.config(essid="ESP8266", authmode=network.AUTH_WPA_WPA2_PSK, password="xcubae00")
-    
-import machine
-pin = machine.Pin(2, machine.Pin.OUT)
-
-index = open('index.html')
-html = index.read()
-index.close()
-
-import socket
 addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
-
 s = socket.socket()
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind(addr)
 s.listen(1)
 
-print('listening on', addr)
+pin = machine.Pin(2, machine.Pin.OUT)
 
-import os
+with open("index.html") as f:
+    html = f.read()
+    systemData = os.uname()
+    systemInfo = """
+    {}
+    <ul>
+        <li>System: {}</li>
+        <li>Release: {}</li>
+        <li>MicroPython: {}</li>
+    </ul>
+    """.format(systemData[4], systemData[0], systemData[2], systemData[3])
+    html = html.replace("$SYSTEM_INFO", systemInfo, 1)
+    f.close()
 
-def generateHTTPok(length):
-    response = "HTTP/1.0 200 OK\r\n"
-    response += "Content-Length: " + str(length) + "\r\n"
-    response += "Content-Type: text/plain\r\n\r\n"
-    return response
+def handleAsset(what, client):
+    try:
+        with open(what) as f:
+            while True:
+                chunk = f.read(256)
+                if chunk:
+                    client.write(chunk)
+                else:
+                    break
+            f.close()
+    except Exception as e:
+        print(e)
 
 def handleGET(what, rest, client):
     while True:
         line = rest.readline()
         if not line or line == b'\r\n':
             break
-
     what = what[1:].decode("utf-8")
-    print("Handling GET request on '{}'".format(what))
-    
-    # handle assets
-    if what.startswith("css") or what == "favicon.ico":
-        try:
-            with open(what) as f:
-                response = generateHTTPok(os.stat(what)[6])
-                client.write(response)
-                while True:
-                    chunk = f.read(256)
-                    if chunk:
-                        client.write(chunk)
-                    else:
-                        break
-                f.close()
-        except Exception as e:
-            print(e)
-        return
-    
-    # generate index
-    row = '<tr><td>{}</td><td>{}</td></tr>'.format(pin, pin.value())
-    response = html.replace("$TABLE", row)
-    client.write(response)
+    if what.startswith("TOOGLE_LED"):
+        pin.value(not pin.value())
+    elif what.startswith("TOOGLE_BLINK"):
+        pin.value(not pin.value())
+    elif what.endswith("css") or what.endswith("ico"):
+        return handleAsset(what, client)
+    res = html.replace("$LED_STATUS", "OFF" if pin.value() else "ON", 1)
+    res = res.replace("$BLINK_STATUS", "OFF" if pin.value() else "ON", 1)
+    client.write(res)
 
-
-# main server loop
 while True:
-    # accept connection
     cl, addr = s.accept()
-    print('client connected from', addr)
-    # read the request
     data = cl.makefile('rwb', 0)
-    # read the request header
     line = data.readline()
     tokens = line.split()
     if len(tokens) < 2:
         continue
-    # generate response
     response = None
-    print(tokens)
     if tokens[0] == b'GET':
         handleGET(tokens[1], data, cl)
     cl.close()
